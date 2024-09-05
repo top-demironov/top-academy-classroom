ee2def44834239d6438ff260a86179b437eecf67
top-demironov <den-voin@mail.ru>
2024-09-05 21:49:07 +0700

    Решил 1 задачу

--- lesson1/main.py
+++ lesson1/main.py
@@ -1,1 +1,15 @@
-print('asdf')
+
+text: str = ''
+bad_words: list[str] = []
+
+with open('input.txt', encoding='utf-8') as f:
+	text = f.read()
+
+with open('bad-words.txt', encoding='utf-8') as f:
+	bad_words = f.read().split(' ')
+
+for word in bad_words:
+	text = text.replace(word, len(word) * '*')
+
+with open('output.txt', 'w', encoding='utf-8') as f:
+	f.write(text)
